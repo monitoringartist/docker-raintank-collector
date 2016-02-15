0a1e4dcc089e21f2264f84fd186c2da9179976c5
Jan Garaj <jan.garaj@gmail.com>
2016-02-15 00:01:00 +0000

    Alpine Linux

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM debian:jessie
+FROM alpine
 MAINTAINER Jan Garaj info@monitoringartist.com
 
 ENV \
@@ -6,32 +6,26 @@ ENV \
  numCPUs=1 \
  serverUrl="https://controller.raintank.io" \
  apiKey="" \
- probeServerPort=8284 \
- GOLANG_DOWNLOAD_URL="https://golang.org/dl/go1.5.3.linux-amd64.tar.gz" \
- GOLANG_DOWNLOAD_SHA256=43afe0c5017e502630b1aea4d44b8a7f059bf60d7f29dfd58db454d4e4e0ae53 \
- GOPATH=/go
+ probeServerPort=8284 
  
 COPY docker-image-files /
 
 RUN \
-  apt-get update && \
-  apt-get install -y --no-install-recommends \
-    g++ gcc libc6-dev make curl npm git && \
-  curl -fsSL "$GOLANG_DOWNLOAD_URL" -o golang.tar.gz && \
-  echo "$GOLANG_DOWNLOAD_SHA256 golang.tar.gz" | sha256sum -c - && \
-  tar -C /usr/local -xzf golang.tar.gz && \
-  rm golang.tar.gz && \
-  export PATH=$GOPATH/bin:/usr/local/go/bin:$PATH && \
-  mkdir -p "$GOPATH/src" "$GOPATH/bin" && chmod -R 777 "$GOPATH" && \
+  export GOPATH=/go && \
+  apk update && \
+  apk add -f \
+    g++ gcc make curl nodejs git go && \
+  mkdir -p "$GOPATH/src" "$GOPATH/bin" && chmod -R 777 "$GOPATH" && \  
   mkdir -p /opt/raintank && \
   cd /opt/raintank && \  
   git clone https://github.com/raintank/raintank-collector.git && \
   go get github.com/raintank/raintank-probe && \
-  cd raintank-collector && cp $(which raintank-probe) . && \
+  cd raintank-collector && \
+  cp $GOPATH/bin/raintank-probe . && \
   npm install && \
   chmod +x /bootstrap.sh && \
-  apt-get autoremove -y g++ gcc make curl git && \
-  rm -rf /usr/local/go/ /go/ /opt/raintank/raintank-collector/*.md /var/lib/apt/lists/* && \
+  apk del --purge g++ gcc make curl git go && \
+  rm -rf /var/cache/apk/* /opt/raintank/raintank-collector/*.md ${GOPATH} && \
   rm -rf /opt/raintank/raintank-collector/circle.yml /opt/raintank/raintank-collector/etc && \
   rm -rf /opt/raintank/raintank-collector/.git* /opt/raintank/raintank-collector/.npmignore
 
--- docker-image-files/bootstrap.sh
+++ docker-image-files/bootstrap.sh
@@ -1,31 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
-set -eu
-export TERM=xterm
-red=$(tput setaf 1)
-green=$(tput setaf 2)
-yellow=$(tput setaf 3)
-white=$(tput setaf 7)
-bold=$(tput bold)
-reset=$(tput sgr0)
+# TODO yellow color
 
 if [ -z "$name" ]; then
     export name=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 fi
-printf %s "${yellow}[" "$(date +"%Y-%m-%d %T.")" "$(date +"%N"|cut -c -3)" "] [INFO] /boostrap.sh -" " Config name: $name${reset}"
+printf %s "${yellow}[" "$(date +"%Y-%m-%d %T.")" "000] [INFO] /boostrap.sh -" " Config name: $name${reset}"
 echo ""
-printf %s "${yellow}[" "$(date +"%Y-%m-%d %T.")" "$(date +"%N"|cut -c -3)" "] [INFO] /boostrap.sh -" " Config numCPUs: $numCPUs${reset}"
+printf %s "${yellow}[" "$(date +"%Y-%m-%d %T.")" "000] [INFO] /boostrap.sh -" " Config numCPUs: $numCPUs${reset}"
 echo ""
-printf %s "${yellow}[" "$(date +"%Y-%m-%d %T.")" "$(date +"%N"|cut -c -3)" "] [INFO] /boostrap.sh -" " Config serverUrl: $serverUrl${reset}"
+printf %s "${yellow}[" "$(date +"%Y-%m-%d %T.")" "000] [INFO] /boostrap.sh -" " Config serverUrl: $serverUrl${reset}"
 echo ""
-printf %s "${yellow}[" "$(date +"%Y-%m-%d %T.")" "$(date +"%N"|cut -c -3)" "] [INFO] /boostrap.sh -" " Config apiKey: $apiKey${reset}"
+printf %s "${yellow}[" "$(date +"%Y-%m-%d %T.")" "000] [INFO] /boostrap.sh -" " Config apiKey: $apiKey${reset}"
 echo ""
-printf %s "${yellow}[" "$(date +"%Y-%m-%d %T.")" "$(date +"%N"|cut -c -3)" "] [INFO] /boostrap.sh -" " Config probeServerPort: $probeServerPort${reset}"
+printf %s "${yellow}[" "$(date +"%Y-%m-%d %T.")" "000] [INFO] /boostrap.sh -" " Config probeServerPort: $probeServerPort${reset}"
 echo ""
-for i in $( set -o posix ; set ); do
+for i in $( set ); do
     reg=$(echo ${i} | awk -F'=' '{print $1}')
-    val=$(echo ${i} | awk -F'=' '{print $2}')
+    val=$(echo ${i} | awk -F'=' '{print $2}' | tr -d "'")
     sed -i "s#\$${reg}#${val}#g" /etc/raintank/config-probe.json
 done
 cd /opt/raintank/raintank-collector
-nodejs app.js -c /etc/raintank/config-probe.json
+node app.js -c /etc/raintank/config-probe.json
